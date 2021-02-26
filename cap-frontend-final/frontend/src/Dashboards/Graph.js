440205e0f23c7bfbcdff175bf9a611e926bb3d2c
prajapatilalit <prajapatilalit150992@gmail.com>
2021-02-26 11:02:38 +0530

    graph style update

--- cap-frontend-final/frontend/src/Dashboards/Graph.js
+++ cap-frontend-final/frontend/src/Dashboards/Graph.js
@@ -3,8 +3,24 @@ import React, { useState, useEffect } from "react";
 import { addGraphValues, getPatGraph } from "../CallingApi/patientapi";
 import Chart from "./chart";
 import "./styles.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function Graph({ history }) {
+  const classes = useStyles();
   const doc = JSON.parse(localStorage.getItem("jwt"));
   const doctorName = doc.user.doctor_name;
   const UID = history.location.state.userinfo.UID;
@@ -80,96 +96,135 @@ function Graph({ history }) {
   };
 
   return (
-    <div>
-      <div>
-        <h1>Add Graph here</h1>
-        <Link to="/doctor/dashboard">
-          {" "}
-          <button>Go back To Dashboard</button>{" "}
-        </Link>
-        <button
-          onClick={() => {
-            history.push("/doctor/AddingFeatures", history.location.state);
-          }}
-        >
-          Go back Upload Panal
-        </button>
-        <br></br>
-        <br></br>
-        <br></br>
-        <input
-          placeholder="Heart_rate"
-          name="Heart_rate"
-          onChange={onChange}
-          value={Heart_rate}
-        ></input>
-        <br></br>
-        <input
-          placeholder="Blood_pressure"
-          name="Blood_pressure"
-          onChange={onChange}
-          value={Blood_pressure}
-        ></input>
-        <br></br>
-        <input
-          placeholder="Cholesterol"
-          name="Cholesterol"
-          onChange={onChange}
-          value={Cholesterol}
-        ></input>
-        <br></br>
-        <input
-          placeholder="Blood_sugar"
-          name="Blood_sugar"
-          onChange={onChange}
-          value={Blood_sugar}
-        ></input>
-        <br></br>
-        <br></br>
-        <button onClick={onClicks}>Submit </button>
-        {result ? <p style={{ color: "green" }}>{result}</p> : ""}
-        {/* {JSON.stringify(resultValues)}
-             {JSON.stringify(values)} */}
-        {/* {JSON.stringify(graphValues)} */}
-      </div>
-      <div className="chart_box">
-        <div>
-          <div>
-            <Chart
-              className="chart"
-              labels={dateArr}
-              name={"Heart Rate"}
-              dataVal={heart_rate}
-            />
-          </div>
-          <div>
-            <Chart
-              className="chart"
-              labels={dateArr}
-              name={"Blood Pressure"}
-              dataVal={blood_pressure}
-            />
-          </div>
-        </div>
-        <div>
-          <div>
-            <Chart
-              className="chart"
-              labels={dateArr}
-              name={"Cholesterol"}
-              dataVal={cholesterol}
-            />
-          </div>
-          <div>
-            <Chart
-              className="chart"
-              labels={dateArr}
-              name={"Blood Bugar"}
-              dataVal={blood_sugar}
-            />
-          </div>
-        </div>
-      </div>
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>
+            <h1 className="prescription_heading">Add Graph here</h1>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6} alignContent={"center"}>
+          <Paper
+            style={{ height: "100%", alignItems: "center" }}
+            className={classes.paper}
+          >
+            <div style={{ marginTop: "80px" }}>
+              <div>
+                <Link to="/doctor/dashboard">
+                  <Button variant="contained" color="primary" size="large">
+                    Go back To Dashboard
+                  </Button>
+                </Link>
+              </div>
+              <div>
+                <Button
+                  style={{ margin: "25px" }}
+                  variant="contained"
+                  color="primary"
+                  size="large"
+                  onClick={() => {
+                    history.push(
+                      "/doctor/AddingFeatures",
+                      history.location.state
+                    );
+                  }}
+                >
+                  Go back Upload Panal
+                </Button>
+              </div>
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <div className="form_field">
+              <input
+                placeholder="Heart_rate"
+                name="Heart_rate"
+                // onChange={onChange}
+                // value={Heart_rate}
+              ></input>
+              <br></br>
+              <input
+                placeholder="Blood_pressure"
+                name="Blood_pressure"
+                // onChange={onChange}
+                // value={Blood_pressure}
+              ></input>
+              <br></br>
+              <input
+                placeholder="Cholesterol"
+                name="Cholesterol"
+                // onChange={onChange}
+                // value={Cholesterol}
+              ></input>
+              <br></br>
+              <input
+                placeholder="Blood_sugar"
+                name="Blood_sugar"
+                // onChange={onChange}
+                // value={Blood_sugar}
+              ></input>
+              <br></br>
+              <br></br>
+
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className="ml30"
+              >
+                Submit{" "}
+              </Button>
+
+              {/* {result ? <p style={{ color: "green" }}>{result}</p> : ""} */}
+            </div>
+          </Paper>
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <div>
+              <Chart
+                className="chart"
+                labels={dateArr}
+                name={"Heart Rate"}
+                dataVal={heart_rate}
+              />
+            </div>
+            <div>
+              <Chart
+                className="chart"
+                labels={dateArr}
+                name={"Blood Pressure"}
+                dataVal={blood_pressure}
+              />
+            </div>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <div>
+              <div>
+                <Chart
+                  className="chart"
+                  labels={dateArr}
+                  name={"Cholesterol"}
+                  dataVal={cholesterol}
+                />
+              </div>
+              <div>
+                <Chart
+                  className="chart"
+                  labels={dateArr}
+                  name={"Blood Bugar"}
+                  dataVal={blood_sugar}
+                />
+              </div>
+            </div>
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- cap-frontend-final/frontend/src/Dashboards/chart.js
+++ cap-frontend-final/frontend/src/Dashboards/chart.js
@@ -1,46 +1,47 @@
-import React from 'react'
-import { Line } from 'react-chartjs-2'
+import React from "react";
+import { Line } from "react-chartjs-2";
 
 const Chart = (props) => {
   // console.log(data);
-  const name = props.name
-  const labels = props.labels
-  const dataVal = props.dataVal
+  const name = props.name;
+  const labels = props.labels;
+  const dataVal = props.dataVal;
   const Chartdata = {
     labels: labels,
     datasets: [
       {
         label: props.name,
+        fill: "false",
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
         ],
         borderWidth: 1,
         data: dataVal,
       },
     ],
-  }
+  };
 
   return (
     <div>
       <div
         style={{
-          width: '90%',
-          height: '400px',
-          display: 'inline-flex',
-          backgroundColor: 'antiquewhite',
-        }}>
+          width: "90%",
+          height: "400px",
+          display: "inline-flex",
+          backgroundColor: "antiquewhite",
+        }}
+      >
         <Line
           data={Chartdata}
-          type='line'
           options={{
             maintainAspectRatio: false,
             title: {
@@ -54,7 +55,7 @@ const Chart = (props) => {
                   ticks: {
                     beginAtZero: true,
                     fontSize: 15,
-                    fontColor: '#000',
+                    fontColor: "#000",
 
                     stepSize: 5,
                   },
@@ -64,15 +65,16 @@ const Chart = (props) => {
                 {
                   ticks: {
                     fontSize: 15,
-                    fontcolor: '#000',
+                    fontcolor: "#000",
                   },
                 },
               ],
             },
-          }}></Line>
+          }}
+        ></Line>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chart
+export default Chart;
